ff804e360a7edf03da083f58b5dad1380308b9c0
Jupazago <juan.zapata103@udea.edu.co>
2020-10-17 16:08:57 -0500

    modificacion de valores
    modificar valores  de cada dato de los cañones

--- Parcial/main.cpp
+++ Parcial/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <map>
 
 using namespace std;
 
 
 void llenar_datos(int *datos, int);
+bool golpear(int *datosC1, int *datosC2);
+void modificar_datos(int *datos, int numero);
 
 
 int main()
@@ -17,9 +16,15 @@ int main()
 
     llenar_datos(&datosC1[0], 1);
     cout <<endl<< "agregado ofensivo con exito"<<endl;
-    llenar_datos(&datosC2[0], 2);
+
+    llenar_datos(datosC2, 2);
     cout <<endl<< "Agregado defensivo con exito"<<endl;
 
+    bool derribado=false;
+
+    while (derribado==false) {
+        derribado = golpear(datosC1, datosC2);
+    }
 
     return 0;
 }
@@ -49,6 +54,49 @@ void llenar_datos(int *datos, int numero){
     }
 }
 
+bool golpear(int *datosC1, int *datosC2){
+
+    modificar_datos(datosC1, 1);
+    modificar_datos(datosC2, 2);
+}
+
+void modificar_datos(int *datos, int numero){
+    int angulo, vel_inicial;
+    while(numero==1){
+        cout <<"que angulo de tiro sera para A, de 1 hasta 89 grados"<<endl;
+        cin >> angulo;
+        if(angulo>1 && angulo<90){
+            datos[2] = angulo;
+            cout <<"Que velocidad inicial tiene el canion"<<endl;
+            cin >> vel_inicial;
+            if(vel_inicial>0){
+                datos[3] = vel_inicial;
+            }else{
+                cout <<"Error: velocidad inicial erronea, debe ser mayor a 0"<<endl;
+            }
+        }else{
+            cout <<"Error, Agrega un valor valido"<<endl;
+        }
+    }
+
+    while(numero==1){
+        cout <<"que angulo de tiro sera para A, de 1 hasta 89 grados"<<endl;
+        cin >> angulo;
+        if(angulo>1 && angulo<90){
+            datos[2] = angulo+90;
+            cout <<"Que velocidad inicial tiene el canion"<<endl;
+            cin >> vel_inicial;
+            if(vel_inicial>0){
+                datos[3] = vel_inicial;
+            }else{
+                cout <<"Error: velocidad inicial erronea, debe ser mayor a 0"<<endl;
+            }
+        }else{
+            cout <<"Error, Agrega un valor valido"<<endl;
+        }
+    }
+
+}
 
 
 
